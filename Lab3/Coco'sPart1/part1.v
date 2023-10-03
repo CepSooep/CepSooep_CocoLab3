3a2015c93d3090dbbcfb8170de3c317a111a80f2
cocolu76 <116968267+cocolu76@users.noreply.github.com>
2023-10-03 13:57:12 -0400

    Add files via upload

--- Lab3/Coco'sPart1/part1.v
+++ Lab3/Coco'sPart1/part1.v
@@ -2,7 +2,7 @@ module part1(a, b, c_in, s, c_out);
 	input [3:0] a, b;
 	input c_in;
 	output [3:0] s;
-	output c_out;
+	output [3:0] c_out;
 	
 	wire c1, c2, c3;
 	
@@ -11,10 +11,14 @@ module part1(a, b, c_in, s, c_out);
 	assign s[2] = c2 ^ (a[2]^b[2]);
 	assign s[3] = c3 ^ (a[3]^b[3]);
 	
+	wire cout;
+
 	assign c1 = a[0]&b[0] | c_in&b[0] | c_in&a[0];
 	assign c2 = a[1]&b[1] | c1&b[1] | c1&a[1];
 	assign c3 = a[2]&b[2] | c2&b[2] | c2&a[2];
-	assign c_out = a[3]&b[3] | c3&b[3] | c3&a[3];
+	assign cout = a[3]&b[3] | c3&b[3] | c3&a[3];
+
+	assign c_out = {cout, c3, c2, c1};
 	
 endmodule
 
@@ -29,14 +33,14 @@ module part1test(SW, LEDR);
 	input [8:0] SW;
 	output [9:0] LEDR;
 
-	wire [3:0] A, B, S;
+	wire [3:0] A, B, S, cout;
 	assign A = SW[7:4];
 	assign B = SW[3:0];
-	wire cin, cout;
+	wire cin;
 	assign cin = SW[8];
 	
 	part1 U1 (.a(A), .b(B), .c_in(cin), .s(S), .c_out(cout));
 	assign LEDR[3:0] = S;
-	assign LEDR[9] = cout;
+	assign LEDR[9:6] = cout;
 endmodule
 
--- Lab3/Coco'sPart1/wavepart1.do
+++ Lab3/Coco'sPart1/wavepart1.do
@@ -13,36 +13,51 @@ log {/*}
 # add wave {/*} would add all items in top level simulation module
 add wave {/*}
 
+#expect 1,1111
+force {a} 4'b1111
+force {b} 4'b1111
+force {c_in} 1
+#run simulation for a few ns
+run 40ns
 
-# c:0 s:0111
+#expect 0,0001
+force {a} 4'b0000
+force {b} 4'b0000
+force {c_in} 1
+#run simulation for a few ns
+run 40ns
+
+#expect 0,0000
+force {a} 4'b0000
+force {b} 4'b0000
+force {c_in} 0
+#run simulation for a few ns
+run 40ns
+
 force {a} 4'b0100
 force {b} 4'b0011
 force {c_in} 0
 #run simulation for a few ns
 run 40ns
 
-# c:0 s:1010
 force {a} 4'b1000
 force {b} 4'b0010
 force {c_in} 0
 #run simulation for a few ns
 run 40ns
 
-#c:1 s:0010
 force {a} 4'b1111
 force {b} 4'b0011
 force {c_in} 0
 #run simulation for a few ns
 run 40ns
 
-#c:1 s:1001
 force {a} 4'b1101
 force {b} 4'b1100
 force {c_in} 0
 #run simulation for a few ns
 run 40ns
 
-#c:0 s:0111
 force {a} 4'b0111
 force {b} 4'b0000
 force {c_in} 0
