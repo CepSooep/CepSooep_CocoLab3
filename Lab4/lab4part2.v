908feea9caddbd6490a6a02fcfd95029e23bebd3
CepSooep <143263092+CepSooep@users.noreply.github.com>
2023-10-18 14:56:00 -0400

    Add files via upload

---
+++ Lab4/lab4part2.v
@@ -0,0 +1,38 @@
+module four_bit_register (input [7:0] dIn, input clk, input reset, output reg[7:0] qOut); 
+    wire [7:0] d;
+    wire clkW;
+    assign d = dIn; 
+
+    always@ (posedge clk) begin
+        if (reset)
+            qOut <= 8'b00000000;
+        else   
+            qOut <= dIn;
+    end
+endmodule
+
+module part2 (Clock, Reset_b, Data, Function, ALUout);
+
+    input [3:0] Data;
+    input Clock;
+    input Reset_b;
+    input [1:0] Function;
+    output [7:0] ALUout;
+
+    reg [7:0] q;
+
+    always @(*)
+    begin
+        case (Function)
+            0: q = Data + ALUout[3:0];
+            1: q = Data * ALUout[3:0]; 
+            2: q = ALUout << Data;  
+            3: q = {ALUout[7:0]};
+            default: q = 8'b00000000;
+        endcase
+    end
+
+    four_bit_register reg1(.dIn(q), .clk(Clock), .reset(Reset_b), .qOut(ALUout));
+
+endmodule
+
---
+++ Lab4/lab4part3.v
@@ -0,0 +1,58 @@
+module part3(clock, reset, ParallelLoadn, RotateRight, ASRight, Data_IN, Q);
+    input clock, reset, ParallelLoadn, RotateRight, ASRight;
+    input [3:0] Data_IN;
+    output [3:0] Q;
+
+    wire [3:0] qWire;
+    reg [3:0] qReg;
+    wire rotateLeft;
+    assign rotateLeft = ~RotateRight;
+    wire [3:0] choosenQ;
+
+    wire [3:0] givenD;
+
+    always@ (posedge clock or posedge reset) begin
+        if(reset)
+            qReg <= 0;
+        else if (!ParallelLoadn)
+            qReg <= Data_IN;
+        else if(!ASRight)begin
+            if (RotateRight) begin
+                qReg[3] <= Q[0];
+                qReg[2] <= Q[3];
+                qReg[1] <= Q[2];
+                qReg[0] <= Q[1];
+            end
+            else begin
+                qReg[3] <= Q[2];
+                qReg[2] <= Q[1];
+                qReg[1] <= Q[0];
+                qReg[0] <= Q[3];
+                
+            end
+        end
+        else if(ASRight) begin
+            if(RotateRight) begin
+                qReg[2] <= Q[3];
+                qReg[1] <= Q[2];
+                qReg[0] <= Q[1];
+            end
+            else if (!RotateRight) begin
+                qReg[3] <= Q[2];
+                qReg[2] <= Q[1];
+                qReg[1] <= Q[0];
+                qReg[0] <= Q[3];
+            end
+        end
+    end
+
+    assign Q = qReg;
+endmodule
+
+            
+                
+
+
+
+
+    
