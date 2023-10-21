28ae20ee43ae477e10ee151af2522a7d503a7af8
cocolu76 <116968267+cocolu76@users.noreply.github.com>
2023-10-21 17:56:40 -0400

    Update part2.v

--- Lab5/part2.v
+++ Lab5/part2.v
@@ -1,3 +1,31 @@
+module part2board (CLOCK_50, SW[9], SW[1:0], HEX0);
+	input CLOCK_50;
+	input [9:0] SW;
+	output [6:0] HEX0;
+
+	wire [3:0]hexWire;
+	part2 MAIN(.ClockIn(CLOCK_50), .Reset(SW[9]), .Speed(SW[1:0]), .CounterValue(hexWire));
+	hex H0(hexWire, HEX0);
+endmodule
+
+module part2 #(parameter CLOCK_FREQUENCY = 50000000)(input ClockIn, input Reset, input [1:0] Speed, output [3:0] CounterValue);
+	wire EnableDC;
+	RateDivider RD(.ClockIn(ClockIn), .Reset(Reset), .Speed(Speed), .Enable(EnableDC));
+	DisplayCounter CD(.Clock(ClockIn), .Reset(Reset), .EnableDC(EnableDC), .CounterValue(CounterValue));
+endmodule
+
+module DisplayCounter (input Clock,input Reset,input EnableDC,output [3:0] CounterValue);
+	 //4bit
+	reg [3:0] CounterRegOut;
+	
+	always @(posedge Clock) begin
+		if(Reset) CounterRegOut <= 4'b0000;
+		else if(EnableDC == 1) CounterRegOut <= CounterRegOut + 1;
+		else CounterRegOut <= CounterRegOut;
+	end
+	assign CounterValue = CounterRegOut;
+endmodule
+
 module RateDivider #(parameter CLOCK_FREQUENCY = 50000000) (input ClockIn, input Reset, input [1:0] Speed, output Enable);
     
     reg [($clog2(4*CLOCK_FREQUENCY)-1):0] N; //#of clock cycles per pulse
@@ -9,7 +37,7 @@ module RateDivider #(parameter CLOCK_FREQUENCY = 50000000) (input ClockIn, input
     assign tempStorage2 = CLOCK_FREQUENCY/0.5;
     assign tempStorage3 = CLOCK_FREQUENCY/0.25;
 	
-    reg [$clog2(MAXN)-1:0] counter; //reg is max sized right now
+    reg [$clog2(MAXN):0] counter; //reg is max sized right now
 
     always @(posedge ClockIn, posedge Reset) begin
         if(Speed == 2'b00)
@@ -23,15 +51,54 @@ module RateDivider #(parameter CLOCK_FREQUENCY = 50000000) (input ClockIn, input
         end
         
         
-        always @(posedge ClockIn) begin
+        always @(posedge ClockIn, negedge Reset) begin
 
         if(Reset)
-            counter <= {$clog2(MAXN){1'b1}}; 
+            counter <= {($clog2(MAXN)+1){1'b1}}; 
 		//all ones, even the leftmost bits that we dont care about
+	else if(Speed == 2'b00) 
+		counter <= {($clog2(MAXN)+1){1'b0}};
+		// counter is all 0 to trigger enable at each clock tick
+		// it's just always 1, not a pulse - does that work?
 
+	else if(counter[$clog2(N)] == 0) 
+		counter <= {($clog2(MAXN)+1){1'b1}}; 
         else
             counter <= counter - 1;
 
     end
-    assign Enable = (counter == 0)?'1:0;  
+    assign Enable = (counter[$clog2(N)] == 0)?'1:0;  
+endmodule
+    
+
+
+module hex_decoder(c, display);
+	input [3:0] c;
+	output [6:0] display;
+	
+	wire c0, c1, c2, c3; //for the ease of typing (they are not wires)
+	assign c0 = c[0];
+	assign c1 = c[1];
+	assign c2 = c[2];
+	assign c3 = c[3];
+
+	wire [6:0] inverted; //I messed up, everything is inverted
+
+	assign inverted[0] = ~c2&~c0 | ~c3&c2&c0 | c2&c1 | c3&~c2&~c1 | c3&~c0 | ~c3&c1;
+	assign inverted[1] = ~c3&~c1&~c0 | ~c2&~c1 | ~c2&~c0 | ~c3&c1&c0 | c3&~c1&c0;
+	assign inverted[2] = ~c2&~c1 | ~c2&c0 | ~c1&c0 | ~c3&c2 | c3&~c2;
+	assign inverted[3] = ~c3&~c2&~c0 | ~c2&c1&c0 | c2&~c1&c0 | c2&c1&~c0 | c3&~c1;
+	assign inverted[4] = ~c2&~c0 | c1&~c0 | c3&c1 |c3&c2;
+	assign inverted[5] = ~c1&~c0 | ~c3&c2&~c1 | c2&~c0 | c3&~c2 | c3&c1;
+	assign inverted[6] = ~c2&c1 | c1&~c0 | ~c3&c2&~c1 | c3&~c2 | c3&c0;
+	
+	assign display = ~inverted;
+
+endmodule
+
+//*********for testing*************************************
+module hex(SW, HEX);
+	input [3:0] SW;
+	output [6:0] HEX;
+	hex_decoder H1(SW, HEX);	
 endmodule
