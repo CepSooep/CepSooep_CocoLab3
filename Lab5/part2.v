d6aa8efd80420b00b92f1ccb1f1cf1ad54815d87
cocolu76 <116968267+cocolu76@users.noreply.github.com>
2023-10-21 13:30:14 -0400

    Update part2.v
    oh boi

--- Lab5/part2.v
+++ Lab5/part2.v
@@ -1,32 +1,37 @@
 module RateDivider #(parameter CLOCK_FREQUENCY = 50000000) (input ClockIn, input Reset, input [1:0] Speed, output Enable);
-    reg N;
-    wire [3:0]tempStorage;
-    assign tempStorage[0] = CLOCK_FREQUENCY/CLOCK_FREQUENCY;
-    assign tempStorage[1] = CLOCK_FREQUENCY/1;
-    assign tempStorage[2] = CLOCK_FREQUENCY/0.5;
-    assing tempStorage[3] = CLOCK_FREQUENCY/0.25;
+    
+    reg [($clog2(4*CLOCK_FREQUENCY)-1):0] N; //#of clock cycles per pulse
+    
+    parameter MAXN = (4*CLOCK_FREQUENCY);
+    wire [($clog2(4*CLOCK_FREQUENCY)-1):0] tempStorage0,tempStorage1, tempStorage2, tempStorage3;
+    assign tempStorage0 = CLOCK_FREQUENCY/CLOCK_FREQUENCY;
+    assign tempStorage1 = CLOCK_FREQUENCY/1;
+    assign tempStorage2 = CLOCK_FREQUENCY/0.5;
+    assign tempStorage3 = CLOCK_FREQUENCY/0.25;
+	
+    reg [$clog2(MAXN)-1:0] counter; //reg is max sized right now
 
     always @(posedge ClockIn, posedge Reset) begin
         if(Speed == 2'b00)
-            N = tempStorage[0]
+            N = tempStorage0;
         else if(Speed == 2'b01)
-           N = tempStorage[1]
+           N = tempStorage1;
         else if(Speed == 2'b10)
-            N = tempStorage[2];
+            N = tempStorage2;
         else if(Speed == 2'b11)
-            N = tempStorage[3];
+            N = tempStorage3;
+        end
         
         
-        output reg [$clog2(N):0] counter;
-        counter
+        always @(posedge ClockIn) begin
 
         if(Reset)
-            counter <= $clog2(N)'b1;
+            counter <= {$clog2(MAXN){1'b1}}; 
+		//all ones, even the leftmost bits that we dont care about
+
         else
             counter <= counter - 1;
 
     end
-    assign Enable = (counter[$clog2(N)] == 1'b0)?'1:0;  
-
-    end
-    
+    assign Enable = (counter == 0)?'1:0;  
+endmodule
