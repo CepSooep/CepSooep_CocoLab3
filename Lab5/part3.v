eb8a21290b444ad09e04bd0a4726676d11ca0212
CepSooep <143263092+CepSooep@users.noreply.github.com>
2023-10-27 19:52:41 -0400

    Add files via upload

--- Lab5/part3.v
+++ Lab5/part3.v
@@ -1,4 +1,4 @@
-module part3 #(parameter CLOCK_FREQUENCY=50000000)(
+module part3 #(parameter CLOCK_FREQUENCY=500)(
     input wire ClockIn,
     input wire Reset,
     input wire Start,
@@ -10,10 +10,11 @@ module part3 #(parameter CLOCK_FREQUENCY=50000000)(
     reg shift;
     reg bitChange;
     wire [12:0]letterReader;
+    wire [12:0]newBitStore;
     wire halfClock;
 
-    RateDivider cockSpliter(ClockIn, Reset, //put speed selection for 0.5 seconds//, halfClock);
-    shiftReg shifter(letterHolder, Reset, letterReader, Start);
+    RateDivider cockSpliter(ClockIn, Reset, 2'b01, halfClock);
+    shiftReg shifter(letterHolder, halfClock, Reset, letterReader, Start);
     
     always@(Letter)begin
         if(Letter == 3'b000)
@@ -43,10 +44,10 @@ module shiftReg(Pword, halfSecClock, reset_n, Q, shift);
 //it is also reset with reset and is parrallel loaded at the clock posedge
 // should only run with a 1 second period clock. ie a pulse for 0.5 seconds and off for 0.5 seconds.
     input [12:0] Pword;
-    input halfSecClock, reset_n, shift_n;
+    input halfSecClock, reset_n, shift;
     output reg [12:0] Q;
     always @(posedge halfSecClock) begin
-            if(!reset_n)
+            if(reset_n)
                 Q <= 0;
             else if(shift)
                 Q <= Pword;
@@ -68,86 +69,52 @@ module shiftReg(Pword, halfSecClock, reset_n, Q, shift);
         end
 endmodule
 
-module RateDivider #(parameter CLOCK_FREQUENCY = 50000000) (input ClockIn, input Reset, input [1:0] Speed, output Enable);
-    
-    reg [($clog2(4*CLOCK_FREQUENCY)-1):0] N; //#of clock cycles per pulse
-    reg [($clog2(4*CLOCK_FREQUENCY)-1):0] Nholder;
-	 reg [($clog2(4*CLOCK_FREQUENCY)-1):0] Nprev;
+
+
+module RateDivider #(parameter CLOCK_FREQUENCY = 500) (input ClockIn, input Reset, input [1:0] Speed, output Enable);
+
     parameter MAXN = (4*CLOCK_FREQUENCY);
-    wire [($clog2(4*CLOCK_FREQUENCY)-1):0] tempStorage0,tempStorage1, tempStorage2, tempStorage3;
-    assign tempStorage0 = 1;
-    assign tempStorage1 = CLOCK_FREQUENCY;
-    assign tempStorage2 = CLOCK_FREQUENCY*2;
-    assign tempStorage3 = CLOCK_FREQUENCY*4;
 	
     reg [$clog2(MAXN):0] counter; //reg is max sized right now
 
-    always @(*) begin
-        if(Speed == 2'b00)
-            N <= tempStorage0;
-        else if(Speed == 2'b01)
-           N <= tempStorage1;
-        else if(Speed == 2'b10)
-            N <= tempStorage2;
-        else if(Speed == 2'b11)
-            N <= tempStorage3;
-    end
-	
+  
+reg [($clog2(4*CLOCK_FREQUENCY)-1):0] c;
+
+always @(*) begin
+	if (Speed == 2'b00) c <= 1/CLOCK_FREQUENCY;
+	if (Speed == 2'b01) c <= 1;
+	if (Speed == 2'b10) c <= 2;
+	if (Speed == 2'b11) c <= 4;
+end
+
+
+
+
 always @(posedge ClockIn) begin
-        
-	
-	if(Nholder == 1&& ~Reset) begin//enable always high
-		counter <= {($clog2(MAXN)+1){1'b0}};
-
-	end
-	else if(Reset) begin
-            	counter <= {{($clog2(MAXN)){1'b1}}}; 
-		//all ones, even the leftmost bits that we dont care about
-
-	end
-	else if(~Reset&&((Nholder == 1 &&counter[0] == 0)||
-	(Nholder == CLOCK_FREQUENCY&&counter[$clog2(CLOCK_FREQUENCY)] == 0)||
-	(Nholder == CLOCK_FREQUENCY*2 &&counter[$clog2(CLOCK_FREQUENCY*2)] == 0)||
-	(Nholder == CLOCK_FREQUENCY*4 && counter[$clog2(CLOCK_FREQUENCY*4)] == 0))) begin
-		counter <= {{($clog2(MAXN)+1){1'b1}}}; 
-	
-	end
-	else if((Nprev == 1 &&counter[0] == 0)||
-	(Nprev == CLOCK_FREQUENCY&&counter[$clog2(CLOCK_FREQUENCY)] == 0)||
-	(Nprev == CLOCK_FREQUENCY*2 &&counter[$clog2(CLOCK_FREQUENCY*2)] == 0)||
-	(Nprev == CLOCK_FREQUENCY*4 && counter[$clog2(CLOCK_FREQUENCY*4)] == 0)) begin
-		counter <= {($clog2(MAXN)+1){1'b1}};
+        if(Reset && Speed != 2'b00) begin
+            	counter <= {CLOCK_FREQUENCY*c-1};//c is reg 
+		
 		
 	end
+	else if(Reset && Speed == 2'b00) begin
+            	counter <= {0};
+		
+		
+	end
+
+
+	else if(counter == 0 && Speed == 2'b00) 
+            	counter <= {0};
+		
+	
+	else if(counter == 0)
+		counter <= {CLOCK_FREQUENCY*c-1}; 
         
 	else
             counter <= counter - 1;
 
 end
-
-
-always @(*) begin
-		if(Reset) begin
-			Nholder <= N;
-			Nprev <= N;
-		end
-		else if((Nholder == 1 &&counter[0] == 0)||
-	(Nholder == CLOCK_FREQUENCY&&counter[$clog2(CLOCK_FREQUENCY)] == 0)||
-	(Nholder == CLOCK_FREQUENCY*2 &&counter[$clog2(CLOCK_FREQUENCY*2)] == 0)||
-	(Nholder == CLOCK_FREQUENCY*4 && counter[$clog2(CLOCK_FREQUENCY*4)] == 0)) begin
-			Nholder <= N;
-			Nprev <= N;
-		end
-
-	end
-	
-	
-    assign Enable = ((Nholder == 1 &&counter[$clog2(1)] == 0)||
-	(Nholder == CLOCK_FREQUENCY&&counter[$clog2(CLOCK_FREQUENCY)] == 0)||
-	(Nholder == CLOCK_FREQUENCY*2 &&counter[$clog2(CLOCK_FREQUENCY*2)] == 0)||
-	(Nholder == CLOCK_FREQUENCY*4 && counter[$clog2(CLOCK_FREQUENCY*4)] == 0))
-	?ClockIn:0;  
+    assign Enable = (counter != 0)?0:1;  
 endmodule
-	
 
                 
