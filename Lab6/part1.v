1308f62694c6b99443b14bc94f2d5340bf403430
CepSooep <143263092+CepSooep@users.noreply.github.com>
2023-10-30 19:01:06 -0400

    Update part1.v

--- Lab6/part1.v
+++ Lab6/part1.v
@@ -66,3 +66,34 @@ output [3:0] CurState
     assign z = (( y_Q == F ) | ( y_Q == G ) ) ; // Output logic
     assign CurState = y_Q ;
 endmodule
+
+module hex_decoder(c, display);
+	input [3:0] c;
+	output [6:0] display;
+	
+	wire c0, c1, c2, c3; //for the ease of typing (they are not wires)
+	assign c0 = c[0];
+	assign c1 = c[1];
+	assign c2 = c[2];
+	assign c3 = c[3];
+
+	wire [6:0] inverted; //I messed up, everything is inverted
+
+	assign inverted[0] = ~c2&~c0 | ~c3&c2&c0 | c2&c1 | c3&~c2&~c1 | c3&~c0 | ~c3&c1;
+	assign inverted[1] = ~c3&~c1&~c0 | ~c2&~c1 | ~c2&~c0 | ~c3&c1&c0 | c3&~c1&c0;
+	assign inverted[2] = ~c2&~c1 | ~c2&c0 | ~c1&c0 | ~c3&c2 | c3&~c2;
+	assign inverted[3] = ~c3&~c2&~c0 | ~c2&c1&c0 | c2&~c1&c0 | c2&c1&~c0 | c3&~c1;
+	assign inverted[4] = ~c2&~c0 | c1&~c0 | c3&c1 |c3&c2;
+	assign inverted[5] = ~c1&~c0 | ~c3&c2&~c1 | c2&~c0 | c3&~c2 | c3&c1;
+	assign inverted[6] = ~c2&c1 | c1&~c0 | ~c3&c2&~c1 | c3&~c2 | c3&c0;
+	
+	assign display = ~inverted;
+
+endmodule
+
+//*********for testing*************************************
+module hex(SW, HEX);
+	input [3:0] SW;
+	output [6:0] HEX;
+	hex_decoder H1(SW, HEX);	
+endmodule
