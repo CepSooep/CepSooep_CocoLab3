c9ef6a832f390592e0689738f588e3f51f6c1618
CepSooep <143263092+CepSooep@users.noreply.github.com>
2023-11-27 19:33:21 -0500

    Update KeyCompiler.v

--- Project/PS2key2/KeyCompiler.v
+++ Project/PS2key2/KeyCompiler.v
@@ -1,37 +1,113 @@
-module TopLevel(
-    // Inputs
+module TopLevel (
+	// Inputs
 	CLOCK_50,
-	KEY[0],
+	KEY,
 
 	// Bidirectionals
 	PS2_CLK,
 	PS2_DAT,
 	
 	// Outputs
-	HEX0
+	HEX0,
+	HEX1,
+	HEX2,
+	HEX3,
+	HEX4,
+	HEX5,
+	HEX6,
+	HEX7
 );
 
-input CLOCK_50;
-input KEY[0];
+/***************************
+ *                           Parameter Declarations                          *
+ ***************************/
 
-inout PS2_CLK;
-inout PS2_DAT;
 
-output HEX0;
+/***************************
+ *                             Port Declarations                             *
+ ***************************/
 
-wire [7:0] last_data_received;
-wire [2:0] dir;
-wire [3:0] outDir;
+// Inputs
+input				CLOCK_50;
+input		[3:0]	KEY;
 
-assign outDir = {1'b0, dir};
+// Bidirectionals
+inout				PS2_CLK;
+inout				PS2_DAT;
 
-keyGet keygetter1 (
-    .CLOCK_50(CLOCK_50),
-    .reset(KEY[0]),
-    .PS2_CLK(PS2_CLK),
-    .PS2_DAT(PS2_DAT),
-    .last_data_received(last_data_received),
-    .dir(dir)
+// Outputs
+output		[6:0]	HEX0;
+output		[6:0]	HEX1;
+output		[6:0]	HEX2;
+output		[6:0]	HEX3;
+output		[6:0]	HEX4;
+output		[6:0]	HEX5;
+output		[6:0]	HEX6;
+output		[6:0]	HEX7;
+
+/***************************
+ *                 Internal Wires and Registers Declarations                 *
+ ***************************/
+
+// Internal Wires
+wire		[7:0]	ps2_key_data;
+wire				ps2_key_pressed;
+wire		[3:0] outDir;
+
+// Internal Registers
+reg			[7:0]	last_data_received;
+
+localparam UPVAL        = 8'b01110011;
+localparam DOWNVAL      = 8'b01110010; 
+localparam LEFTVAL      = 8'b01101001;
+localparam RIGHTVAL     = 8'b01111010;
+
+// State Machine Registers
+
+/***************************
+ *                         Finite State Machine(s)                           *
+ ***************************/
+assign outDir = (last_data_received == UPVAL) ? 3'b001 : (last_data_received == DOWNVAL) ? 3'b010 : (last_data_received == LEFTVAL) ? 3'b011 : (last_data_received == RIGHTVAL) ? 3'b100: 3'b00;
+
+/***************************
+ *                             Sequential Logic                              *
+ ***************************/
+
+always @(posedge CLOCK_50)
+begin
+	if (KEY[0] == 1'b0)
+		last_data_received <= 8'h00;
+	else if (ps2_key_pressed == 1'b1)
+		last_data_received <= ps2_key_data;
+end
+
+/***************************
+ *                            Combinational Logic                            *
+ ***************************/
+assign HEX1 = 7'h7F;
+assign HEX2 = 7'h7F;
+assign HEX3 = 7'h7F;
+assign HEX4 = 7'h7F;
+assign HEX5 = 7'h7F;
+assign HEX6 = 7'h7F;
+assign HEX7 = 7'h7F;
+
+/***************************
+ *                              Internal Modules                             *
+ ***************************/
+
+PS2_Controller PS2 (
+	// Inputs
+	.CLOCK_50				(CLOCK_50),
+	.reset				(~KEY[0]),
+
+	// Bidirectionals
+	.PS2_CLK			(PS2_CLK),
+ 	.PS2_DAT			(PS2_DAT),
+
+	// Outputs
+	.received_data		(ps2_key_data),
+	.received_data_en	(ps2_key_pressed)
 );
 
 Hexadecimal_To_Seven_Segment Segment0 (
@@ -45,68 +121,4 @@ Hexadecimal_To_Seven_Segment Segment0 (
 );
 
 
-
-endmodule
-
-module keyGet(
-    CLOCK_50,
-    reset,
-    PS2_CLK,
-    PS2_DAT,
-    last_data_received,
-    dir
-);
-
-input               CLOCK_50;
-input               reset;
-
-inout               PS2_CLK;
-inout               PS2_DAT;
-
-output reg [7:0]    last_data_received;
-output reg [2:0]    dir;
-
-wire                ps2_key_data;
-wire                ps2_key_pressed;
-
-localparam UPVAL        = 8'b01110011;
-localparam DOWNVAL      = 8'b01110010; 
-localparam LEFTVAL      = 8'b01101001;
-localparam RIGHTVAL     = 8'b01111010;
-
-always @(posedge CLOCK_50)
-begin
-	if (reset == 1'b0)
-		last_data_received <= 8'h00;
-        dir <= 3'b000;
-	else if (ps2_key_pressed == 1'b1) begin
-		last_data_received <= ps2_key_data;
-    end 
-end
-
-always @(last_data_received)
-begin
-    if(last_data_received == UPVAL)
-    dir <= 3'b001;
-    if(last_data_received == DOWNVAL)
-    dir <= 3'b010;
-    if(last_data_received == LEFTVAL)
-    dir <= 3'b011;
-    if(last_data_received == RIGHTVAL)
-    dir <= 3'b100;
-end
-
-PS2_Controller PS2 (
-	// Inputs
-	.CLOCK_50				(CLOCK_50),
-	.reset				(~reset),
-
-	// Bidirectionals
-	.PS2_CLK			(PS2_CLK),
- 	.PS2_DAT			(PS2_DAT),
-
-	// Outputs
-	.received_data		(ps2_key_data),
-	.received_data_en	(ps2_key_pressed)
-);
 endmodule
