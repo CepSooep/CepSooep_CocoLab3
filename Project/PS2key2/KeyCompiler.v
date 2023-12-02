f5eccb2d582c83c25305b1873ba3b9d2af7f1fdb
CepSooep <143263092+CepSooep@users.noreply.github.com>
2023-12-02 04:40:00 -0500

    Add files via upload

--- Project/PS2key2/KeyCompiler.v
+++ Project/PS2key2/KeyCompiler.v
@@ -1,77 +1,162 @@
-module TopLevel (
-	// Inputs
+module TopLevel(
+    // Inputs
 	CLOCK_50,
-	reset,
+	KEY[0],
 
 	// Bidirectionals
 	PS2_CLK,
 	PS2_DAT,
-	outDir
+	
+	// Outputs
+	HEX0,
+	HEX1
 );
 
-/***************************
- *                           Parameter Declarations                          *
- ***************************/
+input CLOCK_50;
+input KEY[0];
+
+inout PS2_CLK;
+inout PS2_DAT;
+
+output HEX0;
+output HEX1;
+
+wire [7:0] last_data_received;
+wire [2:0] dir;
+wire [3:0] outDir;
+wire [2:0] turn;
+wire [3:0] outTurn;
 
 
-/***************************
- *                             Port Declarations                             *
- ***************************/
+assign outDir = {1'b0, dir};
+assign outTurn = {1'b0, turn};
 
-// Inputs
-input				CLOCK_50;
-input				reset;
+keyGet keygetter1 (
+    .CLOCK_50(CLOCK_50),
+    .reset(KEY[0]),
+    .PS2_CLK(PS2_CLK),
+    .PS2_DAT(PS2_DAT),
+    .last_data_received(last_data_received),
+    .forward(dir)
+	.turn(turn)
+);
 
-// Bidirectionals
-inout				PS2_CLK;
-inout				PS2_DAT;
-output 			[3:0] 	outDir;
+Hexadecimal_To_Seven_Segment Segment0 (
+	// Inputs
+	.hex_number			(outDir),
+
+	// Bidirectional
+
+	// Outputs
+	.seven_seg_display	(HEX0)
+);
+Hexadecimal_To_Seven_Segment Segment1 (
+	// Inputs
+	.hex_number			(outTurn),
+
+	// Bidirectional
+
+	// Outputs
+	.seven_seg_display	(HEX1)
+);
 
 
-/***************************
- *                 Internal Wires and Registers Declarations                 *
- ***************************/
 
-// Internal Wires
-wire		[7:0]	ps2_key_data;
-wire			ps2_key_pressed;
+endmodule
+
+module keyGet(
+    CLOCK_50,
+    reset,
+    PS2_CLK,
+    PS2_DAT,
+    last_data_received,
+    forward,
+	turn
+);
+
+input               CLOCK_50;
+input               reset;
+
+inout               PS2_CLK;
+inout               PS2_DAT;
+
+output reg [7:0]    last_data_received;
+output reg [2:0]   	accel;
+output reg [2:0]   	turn;
 
 
-// Internal Registers
-reg			[7:0]	last_data_received;
+wire                ps2_key_data;
+wire                ps2_key_pressed;
 
 localparam UPVAL        = 8'b01110011;
 localparam DOWNVAL      = 8'b01110010; 
 localparam LEFTVAL      = 8'b01101001;
 localparam RIGHTVAL     = 8'b01111010;
-
-// State Machine Registers
-
-/***************************
- *                         Finite State Machine(s)                           *
- ***************************/
-assign outDir = (last_data_received == UPVAL) ? 3'b001 : (last_data_received == DOWNVAL) ? 3'b010 : (last_data_received == LEFTVAL) ? 3'b011 : (last_data_received == RIGHTVAL) ? 3'b100: 3'b00;
-
-/***************************
- *                             Sequential Logic                              *
- ***************************/
+localparam NOVAL		= 8'h00;
 
 always @(posedge CLOCK_50)
 begin
 	if (reset == 1'b0)
 		last_data_received <= 8'h00;
-	else if (ps2_key_pressed == 1'b1)
+        dir <= 3'b000;
+	else if (ps2_key_pressed == 1'b1) begin
 		last_data_received <= ps2_key_data;
+    end 
 end
 
-/***************************
- *                            Combinational Logic                            *
- ***************************/
+always @(last_data_received)
+begin
+	case(last_data_received)
+	NOVAL:
+	begin
+		accel <= accel;
+		turn <= 2'b00;
+	end
+	UPVAL:
+	begin
+		case(accel)
+			2'b00:
+			begin
+				accel <= 2'b01;
+			end
+			
+			2'b01:
+			begin
+				accel <= 2'b00;
+			end
+			2'b10:
+			begin
+				accel <= 2'b01;
+			end
+		endcase
+	end
+	DOWNVAL:
+	begin
+		case(accel)
+		2'b00:
+		begin
+			accel <= 2'b10;
+		end
 
+		2'b10:
+		begin
+			accel <= 2'b00;
+		end
 
-/***************************
- *                              Internal Modules                             *
- ***************************/
+		2'b01:
+		begin
+			accel <= 2'b10;
+		end
+	end
+	LEFTVAL:
+		turn <= 2'b01;
+	
+	RIGHTVAL:
+		turn <= 2'b10;
+		
+	endcase
+
+end
 
 PS2_Controller PS2 (
 	// Inputs
@@ -86,5 +171,4 @@ PS2_Controller PS2 (
 	.received_data		(ps2_key_data),
 	.received_data_en	(ps2_key_pressed)
 );
-
 endmodule
